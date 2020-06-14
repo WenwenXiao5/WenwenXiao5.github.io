8e6cde2585691ae87d75aa6f25293e32a8ebee94
Wenwen Xiao <nicoco@Nicoles-MacBook-Pro.local>
2020-06-13 20:44:55 -0700

    update commit

---
+++ js/master.js
@@ -0,0 +1,8 @@
+function closeme(){
+    x = document.getElementById("test")
+    x.className = "closed";
+}
+function openme(){
+    x = document.getElementById("test")
+    x.className = "open";
+}
